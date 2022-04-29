f2b57f898eedfc6dd376a128f1b3e6516f719445
Sören Metje <soerenmetje@yahoo.de>
2022-04-29 15:16:11 +0200

    Fix #1 distutils package is deprecated and slated for removal in Python 3.12

--- docker/script/extract.py
+++ docker/script/extract.py
@@ -13,7 +13,6 @@ import subprocess
 
 from base64             import b64decode
 from datetime           import datetime, timedelta
-from distutils.util     import strtobool
 from dotenv             import load_dotenv
 from pathlib            import Path
 from watchdog.events    import FileSystemEventHandler
@@ -57,6 +56,21 @@ def r_print ( msg, colorcode=None ):
     global debug
     if debug:
         sys.stderr.write( colorcode + current_dt() + ': ' + msg + COLOR.reset + "\n" )
+
+# convert string to boolean
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
 
 # convert string to boolean
 def bool_val ( str_val ):
